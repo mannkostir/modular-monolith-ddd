86ef0867dd71699db31f3ee2a6b82acf749aa00c
mannkostir <agabatov7@gmail.com>
2023-02-18 18:14:35 +0600

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExceptionInterceptor } from '@lib/interceptors/exception.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ExceptionInterceptor());
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Izumator')
+    .setDescription('Izumator API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      displayRequestDuration: true,
+      filter: true,
+      syntaxHighlight: { activate: true, theme: 'agate' },
+      tryItOutEnabled: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'method',
+    },
+  });
 
   await app.listen(3000);
 }
